f0e0503e33037f87b0edd1563e8db7a56b683809
Nick Buettner <nick.buettner29@gmail.com>
2024-02-24 22:09:27 -0600

    updated to add new parent directory to group all projects in sln together. sln moved up to newe directory as well as .gitignore files

---
+++ Stratosphere.MaintenanceMode.WorkerService/Program.cs
@@ -0,0 +1,7 @@
+using Stratosphere.MaintenanceMode.WorkerService;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
