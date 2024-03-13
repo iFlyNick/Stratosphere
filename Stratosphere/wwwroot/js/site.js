fac928f7f99fbe8fe0765ff3b277fa1b57c92e10
Nick Buettner <nick.buettner29@gmail.com>
2024-03-12 23:52:06 -0500

    continue buildout of main layout functionality. added sidebar data and links, dark mode theme options and solidified the general look and feel of the basic layer of the application outside the content itself

--- Stratosphere/wwwroot/js/site.js
+++ Stratosphere/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('#darkModeToggle').on('click', function () {
+        toggleDarkModeTheme();
+    });
+});
 
-// Write your JavaScript code.
+function toggleDarkModeTheme() {
+    var currentTheme = $('body').attr('data-theme');
+    var newTheme = (currentTheme === 'dark' || currentTheme === undefined || currentTheme === null) ? 'light' : 'dark';
+
+    //update theme and local storage
+    $('body').attr('data-theme', newTheme);
+    setLocalStorageKeyValue('theme', newTheme);
+
+    //update icon
+    setDarkModeIcon(newTheme);
+}
+
+function setDarkModeIcon(theme) {
+    var darkModeIcon = 'bi-moon-stars-fill';
+    var lightModeIcon = 'bi-brightness-high-fill';
+
+    $('#darkModeToggle > span > i').removeClass(`${darkModeIcon} ${lightModeIcon}`).addClass(theme === 'dark' ? darkModeIcon : lightModeIcon);
+}
+
+function setLocalStorageKeyValue(key, val) {
+    localStorage.setItem(key, val);
+}
